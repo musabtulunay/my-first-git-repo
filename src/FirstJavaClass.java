2164c6f0bf20dc2eb427b4e6437d09b1f357a9e4
musabtulunay <109358883+musabtulunay@users.noreply.github.com>
2022-07-17 19:28:30 +0300

    codes without conflict

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,7 +1,5 @@
 public class FirstJavaClass {
-<<<<<<< HEAD
+
     // This is from local Intellij
-=======
-  // This is from remote repository
->>>>>>> 34d50bfc012489db59084083dcef05191b6440e0
+
 }
